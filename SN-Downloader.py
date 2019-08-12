4251e1148ef3552a40a42f011dc4e1631abfbe81
Jason Adamczyk <jason.adamczyk@ICTFM-D012.ictfm.local>
2019-08-12 16:26:06 +0800

    SN downloader added some suggested features as TODO

---
+++ SN-Downloader.py
@@ -0,0 +1,20 @@
+#! python3
+
+# SN-Downloader.py - A script to download content related to the Security Now podcast - https://twit.tv/sn/
+
+# Initial functionality will be to download the show notes from https://www.grc.com/securitynow.htm
+
+
+import requests
+
+# TODO: Scrape the website to get the links to download content
+
+# TODO: Create a Menu to ask the user what files they wish to download
+
+# TODO: Get the number of the latest episode and ask the user how many episodes to download notes for
+
+# TODO: Get Transcripts
+
+# TODO: Get Audio (multiple bitrates)
+
+# TODO: Video - definitely leave until last (large file sizes and better to just stream, I think)
