6ea3a6711046dd335d5f6e330037a41421b5fb2f
Jason Adamczyk <jason.adamczyk@ICTFM-D012.ictfm.local>
2019-08-16 15:20:27 +0800

    Updated twit class with planned functionality

--- twit_show.py
+++ twit_show.py
@@ -12,11 +12,22 @@ class twit():
     hosts = ['Leo Laporte']
     hosts_site = ['https://www.twit.tv']
     show_notes = 'none'
-    dl_links = {
+    dl_types = {    # dict with the type of download as the key and the location of the download as the value
         'HD-Video' : 'none',
         'SD-Video-Lg' : 'none',
         'SD-Video-Sm' : 'none',
         'Audio' : 'none'
         'Youtube' : 'none'
     }
+    latest_ep = 0 # The episode number of the most recenly published podcast, initialised to 0 - constructor to find latest
+    
+    # TODO : Constructor
+
+    # TODO: Get latest episode
+
+    # TODO: Get Episode name from number
+
+    # TODO: Show Episode list (arg specifies how many episodes to show)
+
+    # TODO: Download Content (arg for what type (show notes/audio/video) and what episode number)
 
